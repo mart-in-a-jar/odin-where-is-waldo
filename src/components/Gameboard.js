d59de7d327edb060ec42da020e8d52395ab52b9b
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2023-05-05 12:53:20 +0200

    Better calculations

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -3,12 +3,17 @@ import { getData } from "../firebase";
 
 const Gameboard = ({ image }) => {
     const handeleClick = (e) => {
-        const clickedXPercentage = e.pageX / e.target.width;
-        const clickedYPercentage = e.pageY / e.target.height;
+        // take header into account (and potential other adjustments)
+        const offsetY = e.target.getBoundingClientRect().y + window.scrollY;
+        const offsetX = e.target.getBoundingClientRect().x + window.scrollX;
+        const clickedXPercentage = (e.pageX - offsetX) / e.target.width;
+        const clickedYPercentage = (e.pageY - offsetY) / e.target.height;
         console.log(
             `Clicked X: ${clickedXPercentage}%, Y: ${clickedYPercentage}%`
         );
         getCoords("ash");
+
+        // set coordinates in state, pop open menu at location and handle comparison with the character thats chosen
     };
 
     const getCoords = async (character) => {

d7db8ca0b94b810ccaec87254a1f707310ec4cea
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2023-05-16 14:13:04 +0200

    Style scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -3,14 +3,18 @@ import styles from "./Scoreboard.module.scss";
 
 const Scoreboard = ({ level, time, restart }) => {
     const timeFormated = formatTimeStamp(time / 1000, 2);
+
     return (
         <div className={styles.scoreboard}>
-            <h1>You won!</h1>
-            <h2>Highscores</h2>
-            {/* get times from firestore on current elvel */}
-            <p>Your time: {timeFormated}</p>
-            {/* if time among top 10, let user register score */}
-            <button onClick={restart}>Restart</button>
+            <div>
+                <h1>You won!</h1>
+                <p>Your time: <span className={styles.time}>{timeFormated}</span></p>
+                <button onClick={restart}>Restart</button>
+                <h2>Highscores</h2>
+                {/* get times from firestore on current elvel */}
+                {/* if time among top 10, let user register score */}
+                <p>Comming soon..</p>
+            </div>
         </div>
     );
 };

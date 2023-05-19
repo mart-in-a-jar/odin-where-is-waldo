628ce22159c71a148af3b9de48ccbd06fe4e9ea3
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2023-05-19 15:12:14 +0200

    Remove console logs

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,11 +1,6 @@
 export default function registerServiceWorker() {
     if ("serviceWorker" in navigator) {
         const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
-        navigator.serviceWorker
-            .register(swUrl)
-            .then(() => console.log("Registered SW"))
-            .catch((error) => {
-                console.error("Failed to register SW:", error);
-            });
+        navigator.serviceWorker.register(swUrl);
     }
 }
